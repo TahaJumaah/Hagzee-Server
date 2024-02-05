2fb8fbeef20ad00bb6ca98e62e128ddb0746008f
Taha Jumaah <taharasheed95@gmail.com>
2024-02-05 21:00:37 +0100

    added query to the handler function which takes in a month number, it's 21:00 now so... till next time.

--- controllers/client/getWorkingDays.js
+++ controllers/client/getWorkingDays.js
@@ -1,15 +1,25 @@
 const mongoose = require("mongoose");
-const workingDayModel = require("../../models/workingDaysModel");
+const workingDaysModel = require("../../models/workingDaysModel");
 const connectDB = require("../../lib/connectDB");
 
+
+// Gets a request to a :Provider, with a URL query of 
+// "month", this month is then changed into a date
+//  added and subtracted for the range of the correct length.
+
 async function getWorkingDays(req, res, next) {
+  const queryMonth = new Date(`2024-${req.query.month}-01`)
+  const 
+  const queryMonthUpperRange = new Date(`2024-${req.query.month}-01`)
   connectDB(req.params.provider, process.env.username, process.env.password);
-  const workingDays = await workingDayModel
-    .find({
-      workingDay: { $lt: "2024-02-07T00:00:00.000Z" },
-    })
+  const workingDays = await workingDaysModel
+    .find({$and:[
+      {workingDay: {$gt: queryMonth}},{workingDay:{$lt:queryMonth}}
+    ]})
     .exec();
-  console.log("Got a working days GET request");
+  console.log(queryMonthUpperRange);
+  console.log(req.params)
+  console.log(req.query)
   res.json(workingDays);
 }
 
